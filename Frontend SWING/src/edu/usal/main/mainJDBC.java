9c20e71b084ebad168ebfd67c934b12afc9d41df
LukasArevalo <39845152+LukasArevalo@users.noreply.github.com>
2020-10-09 18:59:34 -0300

    El grando cambio del nombro
    - Cambios los nombres para reflejar una mejor entrega del tp.

---
+++ Frontend SWING/src/edu/usal/main/mainJDBC.java
@@ -0,0 +1,16 @@
+package edu.usal.main;
+
+import edu.usal.vista.consola.VISTAGENERAL;
+
+public class mainJDBC {
+
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		VISTAGENERAL Vista = new VISTAGENERAL();
+	    Vista.MenuPrincipal();
+	}
+
+
+	
+}
