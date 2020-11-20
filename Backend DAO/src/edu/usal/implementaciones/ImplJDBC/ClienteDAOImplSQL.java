2de71d5f8f290fad1ac19eed45e650aa65bf06df
LukasArevalo <39845152+LukasArevalo@users.noreply.github.com>
2020-11-20 20:01:55 -0300

    Termino de arreglar alta cliente
    - Y el consultar
    - consultar todos
    - modificar
    - La alta todavia no funciona :(

--- Backend DAO/src/edu/usal/implementaciones/ImplJDBC/ClienteDAOImplSQL.java
+++ Backend DAO/src/edu/usal/implementaciones/ImplJDBC/ClienteDAOImplSQL.java
@@ -17,102 +17,122 @@ import javax.persistence.Persistence;
 
 public class ClienteDAOImplSQL implements ClienteDAO {
 
-	@Override
-	public Cliente ConsultadeCliente(Cliente IDCliente) {
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("PersistenciaAerolineas");
+    @Override
+    public Cliente ConsultadeCliente(Cliente IDCliente) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("PersistenciaAerolineas");
 
-		//Comienzo la transaccion.
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
+        //Comienzo la transaccion.
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
 
-		Cliente ClienteEncontrado = entityManager.find(Cliente.class,IDCliente.getIDCliente());
-		//Realizo commit!
-		try{
-			entityManager.getTransaction().commit();
-			entityManagerFactory.close();
-			return ClienteEncontrado;
-		} catch(Exception e){
-			System.out.println("No se pudo encontrar el cliente con el ID: "+IDCliente.getIDCliente());
-			entityManagerFactory.close();
-			return null;
-		}
-	}
+        Cliente ClienteEncontrado = entityManager.find(Cliente.class, IDCliente.getIDCliente());
+        //Realizo commit!
+        try {
+            entityManager.getTransaction().commit();
+            entityManagerFactory.close();
+            return ClienteEncontrado;
+        } catch (Exception e) {
+            System.out.println("No se pudo encontrar el cliente con el ID: " + IDCliente.getIDCliente());
+            entityManagerFactory.close();
+            return null;
+        }
+    }
 
-	@Override
-	public List<Cliente> leerTodoCliente() throws IOException, ClassNotFoundException {
+    @Override
+    public List<Cliente> leerTodoCliente() throws IOException, ClassNotFoundException {
 
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("PersistenciaAerolineas");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("PersistenciaAerolineas");
 
-		//Comienzo la transaccion.
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
+        //Comienzo la transaccion.
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
 
-		//Native query para hacer un SELECT *
-		List<Cliente> clientes = entityManager.createQuery("SELECT c FROM Cliente c").getResultList();
-		//Realizo commit!
-		entityManager.getTransaction().commit();
+        //Native query para hacer un SELECT *
+        List<Cliente> clientes = entityManager.createQuery("SELECT c FROM Cliente c").getResultList();
+        //Realizo commit!
+        entityManager.getTransaction().commit();
 
-		entityManagerFactory.close();
-		//Retorno clientes.
-		return clientes;
-	
-	}
+        entityManagerFactory.close();
+        //Retorno clientes.
+        return clientes;
 
-	@Override
-	public void AltaCliente(Cliente alta){
+    }
 
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("PersistenciaAerolineas");
+    @Override
+    public void AltaCliente(Cliente alta) {
 
-		//Genero cliente para guardar y le setteo todos los datos!
-		Cliente save = new Cliente();
-		save.setIDCliente(alta.getIDCliente());
-		save.setApellidoCliente(alta.getApellidoCliente());
-		save.setNombreCliente(alta.getNombreCliente());
-		save.setDni(alta.getDni());
-		save.setRS(alta.getRS());
-		//TODO save.setFechaNacimiento(alta.getFechaNacimiento();
-		save.setMail(alta.getMail());
-		//Comienzo la transaccion.
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		try{
-			entityManager.persist(save);
-			//Realizo commit!
-			entityManager.getTransaction().commit();
-			System.out.println("Se guardo el cliente de forma correcta!");
-		} catch (Exception e){
-			System.out.println("No pude guardar nada hermano!");
-		}
-		entityManagerFactory.close();
-	}
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("PersistenciaAerolineas");
 
-	@Override
-	public void ModCliente(Cliente modificar) throws ClassNotFoundException, IOException {
-	
+        //Genero cliente para guardar y le setteo todos los datos!
+        Cliente save = new Cliente();
+        save.setIDCliente(alta.getIDCliente());
+        save.setApellidoCliente(alta.getApellidoCliente());
+        save.setNombreCliente(alta.getNombreCliente());
+        save.setDni(alta.getDni());
+        save.setRS(alta.getRS());
+        //TODO save.setFechaNacimiento(alta.getFechaNacimiento();
+        save.setMail(alta.getMail());
+        //Comienzo la transaccion.
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            entityManager.persist(save);
+            //Realizo commit!
+            entityManager.getTransaction().commit();
+            System.out.println("Se guardo el cliente de forma correcta!");
+        } catch (Exception e) {
+            System.out.println("No pude guardar nada hermano!");
+        }
+        entityManagerFactory.close();
+    }
+
+    @Override
+    public void ModCliente(Cliente modificar) throws ClassNotFoundException, IOException {
+        //Guardo cliente Actualizado!
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("PersistenciaAerolineas");
+        //Comienzo la transaccion.
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            //Utilizo la live transaction  para hacer el mod
+            Cliente enBaseDeDatos = entityManager.find(Cliente.class,modificar.getIDCliente());
+            enBaseDeDatos.setApellidoCliente(modificar.getApellidoCliente());
+            enBaseDeDatos.setNombreCliente(modificar.getNombreCliente());
+            enBaseDeDatos.setDni(modificar.getDni());
+            enBaseDeDatos.setRS(modificar.getRS());
+            //TODO save.setFechaNacimiento(alta.getFechaNacimiento();
+            enBaseDeDatos.setMail(modificar.getMail());
+            //Realizo commit!
+            entityManager.getTransaction().commit();
+            System.out.println("Se guardo actualizo el cliente " + modificar.getIDCliente() + "  de forma correcta!");
+        } catch (Exception e) {
+            System.out.println("No pude guardar nada hermano!");
+        }
+        entityManagerFactory.close();
+    }
+
+    @Override
+    public void BajaCliente(Cliente baja) throws ClassNotFoundException, IOException {
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("PersistenciaAerolineas");
+
+        //Comienzo la transaccion.
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        //Borro el cliente!
+        entityManager.remove(baja);
+        //Realizo commit!
+        try {
+            entityManager.getTransaction().commit();
+            entityManagerFactory.close();
+            System.out.println("Borre el cliente: "+baja.getIDCliente()+" sin problemas!");
+        } catch (Exception e) {
+            System.out.println("No se pudo borr el cliente con el ID: " + baja.getIDCliente());
+            entityManagerFactory.close();
+        }
+        entityManagerFactory.close();
+
+    }
 
 
-	}
-
-	@Override
-	public void BajaCliente(Cliente baja) throws ClassNotFoundException, IOException {
-	
-		Connection con = ConexionSQLServer.QuispeConexion();
-		Statement st = null;
-		try {
-			
-			st = con.createStatement();
-			String query = "DELETE FROM Cliente WHERE Cliente.IDCliente = '"+baja.getIDCliente()+"'";
-			
-			st.execute(query);
-			st.close();
-			con.close();
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-
-	
 }
